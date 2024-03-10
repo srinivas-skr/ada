17e11603fc267d5fd7fe281a33dd31f93bc3015b
srinivas k <124112058+srinivas-skr@users.noreply.github.com>
2024-03-10 15:40:08 +0530

    Update 01_selection_sort.cpp

--- 01_selection_sort.cpp
+++ 01_selection_sort.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
-#include<chrono>
 using namespace std;
-using namespace chrono;
-// Time Complexity: O(n^2) - Quadratic time complexity due to nested loops
-// Space Complexity: O(1) - Constant space, as the sorting is done in-place
+
 void selectionSort(int arr[], int n) {
   for (int i = 0; i < n - 1; i++) {
     int minIndex = i;
@@ -16,7 +13,6 @@ void selectionSort(int arr[], int n) {
   }
 }
 
-// Function to print the elements of an array
 void printArray(int arr[], int n) {
   for (int i = 0; i < n; i++) {
     cout << arr[i] << " ";
@@ -25,26 +21,15 @@ void printArray(int arr[], int n) {
 }
 
 int main() {
-  // Initializing array with values
   int array[] = {12, 8, 5, 16, 2};
-
-  // Determining the size of the array
   int size = sizeof(array) / sizeof(array[0]);
 
   cout << "Unsorted array: ";
   printArray(array, size);
-    auto start_time = high_resolution_clock::now();
 
-  // Sorting the array using Selection Sort
   selectionSort(array, size);
-  
-   auto stop_time = high_resolution_clock::now();
-  auto duration = duration_cast<nanoseconds>(stop_time - start_time);
 
   cout << "Sorted array: ";
   printArray(array, size);
-  
-  cout << "Time taken by selection Sort: " << duration.count() << " nanoseconds" << endl;
-
   return 0;
 }
