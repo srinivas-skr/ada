f73d64a7dcb24816033130adfab4f27e4021d037
srinivas k <124112058+srinivas-skr@users.noreply.github.com>
2024-03-10 15:40:45 +0530

    Update 01_insertion.cpp

--- 01_insertion.cpp
+++ 01_insertion.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
-#include <chrono>
 using namespace std;
-using namespace chrono;
 
-// Function to perform Insertion Sort on an array
-void insertionSort(int arr[], int n)
-{
+void insertionSort(int arr[], int n) {
   for (int i = 1; i < n; i++) {
     int key = arr[i];
     int j = i - 1;
@@ -18,7 +14,6 @@ void insertionSort(int arr[], int n)
   }
 }
 
-// Function to print the elements of an array
 void printArray(int arr[], int n) {
   for (int i = 0; i < n; i++) {
     cout << arr[i] << " ";
@@ -27,21 +22,15 @@ void printArray(int arr[], int n) {
 }
 
 int main() {
-  // Define the array with values directly
   int size = 5;
   int array[] = {12, 8, 5, 16, 2};
 
   cout << "Unsorted array: ";
   printArray(array, size);
-  // Measure the time taken by the Insertion Sort algorithm
-  auto start_time = high_resolution_clock::now();
-  insertionSort(array, size);
 
-  auto stop_time = high_resolution_clock::now();
-  auto duration = duration_cast<nanoseconds>(stop_time - start_time);
+  insertionSort(array, size);
 
   cout << "Sorted array: ";
   printArray(array, size);
-  cout << "Time taken by Insertion Sort: " << duration.count() << " nanoseconds" << endl;
   return 0;
 }
