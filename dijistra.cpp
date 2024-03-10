26f597170260efe0f0a7b0995b1597e1ee65e5e4
srinivas k <124112058+srinivas-skr@users.noreply.github.com>
2024-03-10 17:00:50 +0530

    Update dijistra.cpp

--- dijistra.cpp
+++ dijistra.cpp
@@ -1,48 +1,51 @@
 #include<iostream>
 using namespace std;
+int n, a[20][20], s[20], d[20], v;
 
-int main() {
-    int n, i, j, a[10][10], d[10], s[10], v, k, min, u;
-
-    cout << "No. of vertices: ";
-    cin >> n;
-    cout << "\nCost matrix: ";
-    for(i = 1; i <= n; i++)
-        for(j = 1; j <= n; j++)
-            cin >> a[i][j];
-
-    cout << "\nSource vertex: ";
-    cin >> v;
-    for(i = 1; i <= n; i++) {
-        s[i] = 0;
+void dijkstra(){
+    int u;
+    // Initialize distances and visited array
+    for(int i = 1; i<=n; i++){
         d[i] = a[v][i];
+        s[i] = 0;
     }
-
     d[v] = 0;
     s[v] = 1;
-    for(k = 2; k <= n; k++) {
-        min = 99;
-        for(i = 1; i <= n; i++) {
-            if(s[i] == 0 && d[i] < min) {
+    
+    // Dijkstra's algorithm
+    for(int k = 2; k<=n; k++){
+        int min = 99;
+        // Find the vertex with the minimum distance from the source among the unvisited vertices
+        for(int i = 1; i<=n; i++)
+            if(s[i]==0 && d[i] < min){
                 min = d[i];
                 u = i;
             }
-        }
         
-
         s[u] = 1;
-        for(i = 1; i <= n; i++) {
-            if(s[i] == 0) {
-                if(d[i] > (d[u] + a[u][i]))
+        // Update distances for vertices adjacent to the selected vertex
+        for(int i = 1; i <= n; i++)
+            if(s[i] == 0){
+                if(d[i] > d[u] + a[u][i])
                     d[i] = d[u] + a[u][i];
             }
-        }
     }
+    cout<<"The shortest path from "<<v<<" is "<<endl;
+    for(int i = 1; i<=n; i++)
+        cout<<v<<"-->"<<i<<"="<<d[i]<<endl;
+}
 
-    cout << "\nShortest distance from " << v << " is " << endl;
-    for(i = 1; i <= n; i++) {
-        if (i != v)
-       cout << v << "--->" << i << "=" << d[i] << endl;
-    }
+int main(){
+    cout<<"Vertices: ";
+    cin>>n;
+    
+    cout<<"\nCost matrix: ";
+    for(int i = 1; i<=n; i++)
+        for(int j = 1; j<=n; j++)
+            cin>>a[i][j];
+    
+    cout<<"\nSource vertex: ";
+    cin>>v;
+    dijkstra();
     return 0;
 }
