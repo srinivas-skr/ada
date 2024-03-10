bc1ceedb789757e1a64d646d8b35a07425e436f8
srinivas k <124112058+srinivas-skr@users.noreply.github.com>
2024-03-10 17:53:06 +0530

    Update 01_selection_sort.cpp

--- 01_selection_sort.cpp
+++ 01_selection_sort.cpp
@@ -21,8 +21,8 @@ void printArray(int arr[], int n) {
 }
 
 int main() {
+  int size = 5;
   int array[] = {12, 8, 5, 16, 2};
-  int size = sizeof(array) / sizeof(array[0]);
 
   cout << "Unsorted array: ";
   printArray(array, size);
