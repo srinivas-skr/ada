219dd8c22d858ed5ba5fc7c2018dca3e7bb440aa
srinivas k <124112058+srinivas-skr@users.noreply.github.com>
2024-03-10 17:33:04 +0530

    Update 08_prim.cpp

--- 08_prim.cpp
+++ 08_prim.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 using namespace std;
-
 int n, c[20][20], i, j, visited[20];
 
 void prim() {
@@ -23,20 +22,27 @@ void prim() {
         visited[b] = 1;
         count++;
     }
-
     cout << "\nTotal cost of MST is " << cost;
 }
 
 int main() {
     cout << "How many vertices? ";
     cin >> n;
-    cout << "\nEnter cost matrix: ";
     
+    cout << "\nEnter cost matrix: ";
     for (i = 1; i <= n; i++) {
         for (j = 1; j <= n; j++)
             cin >> c[i][j];
         visited[i] = 0;
     }
-
     prim();
 }
+
+/* output
+ow many vertices? 4
+Enter cost matrix: 
+0 10 20 0
+10 0 30 5
+20 30 0 15
+0 5 15 0
+*/
