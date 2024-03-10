a1aa0c3d886e5abd050baa0d19a7799bc0894420
srinivas k <124112058+srinivas-skr@users.noreply.github.com>
2024-03-10 16:26:27 +0530

    Update 10_queen.cpp

--- 10_queen.cpp
+++ 10_queen.cpp
@@ -1,34 +1,44 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
 int count = 0, x[100];
 
-bool canPlace(int k, int i) {
-    for (int j = 1; j < k; j++)
-        if (x[j] == i || abs(x[j] - i) == abs(j - k))
-            return false;
-    return true;
+int place(int k,int i){     //checking if we can place kth queen in ith position
+    for(int j = 1; j<k; j++)
+        if((x[j] == i) || (abs(x[j]-i) == abs(j-k)))
+            return 0;
+    return 1;
 }
 
-void nQueen(int k, int n) {
-    for (int i = 1; i <= n; i++)
-        if (canPlace(k, i)) {
+void nQueen(int k, int n){
+    
+    for(int i = 1; i<=n; i++)
+        if(place(k, i)){
             x[k] = i;
-            if (k == n) {
+            if(k == n){
                 count++;
-                for (int j = 1; j <= n; j++, cout << endl)
-                    for (int p = 1; p <= n; p++)
-                        cout << (x[j] == p ? "Q" : "0");
-            } else
-                nQueen(k + 1, n);
+                
+                for(int j = 1; j<=n; j++){
+                    for(int p = 1; p<=n; p++)
+                        if(x[j] == p)
+                            cout<<"Q";
+                        else
+                            cout<<"0";
+                    cout<<endl;
+                }
+            }
+            else
+                nQueen(k+1, n);
         }
+        cout<<endl;
 }
 
-int main() {
+int main(){
     int n;
-    cout << "Enter number of queens: ";
-    cin >> n;
+    cout<<"Enter number of queens: ";
+    cin>>n;
     nQueen(1, n);
-    cout << (count == 0 ? "\nNo solution" : "Number of solutions: " + to_string(count));
-    return 0;
+    if(count == 0)
+        cout<<"\nNo solution";
+    else
+        cout<<"Number of solutions: "<<count;
 }
