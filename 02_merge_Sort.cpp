421bf3cfc1ca05f25adfe076dcfe76f2362e0740
srinivas k <124112058+srinivas-skr@users.noreply.github.com>
2024-03-11 11:51:33 +0530

    Update 02_merge_Sort.cpp

--- 02_merge_Sort.cpp
+++ 02_merge_Sort.cpp
@@ -4,33 +4,33 @@ using namespace std;
 
 void MergeSort(int arr[], int left , int right)
 {
-	if(left < right)
-	{
-		int mid = left + (right - left) / 2;
-		MergeSort(arr, left, mid);
-		MergeSort(arr, mid + 1, right);
-		inplace_merge(arr + left, arr + mid + 1, arr + right + 1); // to merge two sorted sub-arrays
-	}
+    if(left < right)
+    {
+        int mid = left + (right - left) / 2;
+        MergeSort(arr, left, mid);
+        MergeSort(arr, mid + 1, right);
+        inplace_merge(arr + left, arr + mid + 1, arr + right + 1); // to merge two sorted sub-arrays
+    }
 }
 
 void printArray(int arr[], int size)
 {
-	for(int a = 0; a < size; a++)
-	{
-		cout << arr[a] << " " ;
-	}
-	cout << endl;
+    for(int a = 0; a < size; a++)
+    {
+        cout << arr[a] << " " ;
+    }
+    cout << endl;
 }
 
 int main()
 {
-	int arr[] = {15, 5, 24, 8, 1, 3, 16, 10, 20};
-	int n = sizeof(arr) / sizeof(arr[0]);
-	cout << "Original Array: ";
-	printArray(arr, n);
+    int arr[] = {15, 5, 24, 8, 1, 3, 16, 10, 20};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    cout << "Original Array: ";
+    printArray(arr, size); // corrected variable name
 
-	MergeSort(arr, 0, n - 1);
-	cout << "Sorted Array: ";
-	printArray(arr, n);
-	return 0;
+    MergeSort(arr, 0, size - 1); // corrected variable name
+    cout << "Sorted Array: ";
+    printArray(arr, size); // corrected variable name
+    return 0;
 }
