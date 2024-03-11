975488e1e23a409c885e61587d7a93f800230e17
srinivas k <124112058+srinivas-skr@users.noreply.github.com>
2024-03-11 14:00:37 +0530

    Update 02_quick.cpp

--- 02_quick.cpp
+++ 02_quick.cpp
@@ -39,18 +39,16 @@ void printarray(int arr[], int size)
 int main()
 {
     int size;
-    cout << "Enter size of array: ";
+    cout << "Enter size of  the array: ";
     cin >> size;
-
+    
     int arr[size];
     cout << "Enter " << size << " numbers: ";
     for(int i = 0; i < size; i++)
     {
         cin >> arr[i];
     }
-
-    cout << "Original array: ";
-    printarray(arr, size);
+    
     quicksort(arr, 0, size - 1);     // sorted array
     cout << "Sorted array: ";
     printarray(arr, size);
