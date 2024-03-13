3276b16336d6060e50ffe91389dee8f37507bb8b
srinivas k <124112058+srinivas-skr@users.noreply.github.com>
2024-03-14 01:11:55 +0530

    Update 06_warshall.cpp

--- 06_warshall.cpp
+++ 06_warshall.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 using namespace std;
 
-void warshall(int a[20][20], int n);  // Function declaration
+void warshall(int a[20][20], int n);    // Function declaration
 
 int main() {
     int n;
-
     cout << "Enter the number of vertices: ";
     cin >> n;
-
-    int a[20][20];  // Array declaration for adjacency matrix
+ 
+    int a[20][20];        // Array declaration for adjacency matrix
     cout << "Enter the adjacency matrix (0/1):\n";
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -17,23 +16,28 @@ int main() {
         }
     }
     cout << endl;
-    warshall(a, n); // Call the function to compute and print the transitive closure
+    warshall(a, n);       // Call the function to compute and print the transitive closure
     return 0;
 }
 
-// Function definition
-void warshall(int a[20][20], int n) {
+void warshall(int a[20][20], int n) {    // Function definition
     // Applying Warshall's algorithm
     for (int k = 0; k < n; k++)
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 a[i][j] = a[i][j] || (a[i][k] && a[k][j]);
 
-    // Printing the transitive closure matrix
-    cout << "\nTransitive closure matrix: " << endl;
+    cout << "\nTransitive closure matrix: " << endl;    // Printing the transitive closure matrix
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++)
             cout << a[i][j] << "\t";
         cout << endl;
     }
 }
+
+/* output
+Enter the number of vertices: 3
+Enter the adjacency matrix (0/1):
+0 1 0
+0 0 1
+1 0 0*/
