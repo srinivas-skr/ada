4f5f90a781eff9c89c516e8067e9b9ff66e6231a
srinivas k <124112058+srinivas-skr@users.noreply.github.com>
2024-03-13 09:38:19 +0530

    Update 01_selection_sort.cpp

--- 01_selection_sort.cpp
+++ 01_selection_sort.cpp
@@ -25,7 +25,7 @@ int main() {
   cin >> size;
 
   int array[size];
-  cout << "Enter " << size << " elements: ";
+  cout << "Enter elements: ";
   for (int i = 0; i < size; i++) {
     cin >> array[i];
   }
