80f2f48684938042abf182d87d70d9dce623f66d
srinivas k <124112058+srinivas-skr@users.noreply.github.com>
2024-03-14 00:33:38 +0530

    Update 06_floyd.cpp

--- 06_floyd.cpp
+++ 06_floyd.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 using namespace std;
 
-const int INF = 999;
+const int INF = 999; // Constant representing infinity
 
 int main() {
     int distances[10][10], n;
 
-    cout << "Enter the number of cities: \n";
+    cout << "Enter the number of cities: ";
     cin >> n;
-
+    
     cout << "Enter the distances between cities (use 999 for infinity and 0 for loop):\n";
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= n; j++)
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
             cin >> distances[i][j];
 
-    for (int k = 1; k <= n; k++)
-        for (int i = 1; i <= n; i++)
-            for (int j = 1; j <= n; j++)
+    // Floyd-Warshall algorithm to find the shortest paths between cities
+    for (int k = 0; k < n; k++)
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
                 distances[i][j] = min(distances[i][j], distances[i][k] + distances[k][j]);
 
     cout << "Shortest paths between cities:\n";
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++)
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++)
             cout << distances[i][j] << "\t";
         cout << "\n";
     }
@@ -30,9 +31,9 @@ int main() {
 
 /*output 
 Enter the number of cities: 4
-Enter the distances between cities (use 999 for infinity):
-0 3 6 15
-3 0 2 999
-6 2 0 1
-15 999 1 0
+Enter the distances between cities (use 999 for infinity and 0 for loop):
+0 2 999 10
+999 0 3 999
+999 999 0 7
+999 999 999 0
 */
