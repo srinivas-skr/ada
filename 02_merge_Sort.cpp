b06fb62356f9fcd62b37ee1ba483621f77e3be54
srinivas k <124112058+srinivas-skr@users.noreply.github.com>
2024-03-16 16:15:42 +0530

    Update 02_merge_Sort.cpp

--- 02_merge_Sort.cpp
+++ 02_merge_Sort.cpp
@@ -1,55 +1,77 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
 
-void merge(int a[], int low, int mid, int high);
-
-void mergeSort(int a[], int low, int high) {
-    if (low < high) {
-        int mid = (low + high) / 2;
-        mergeSort(a, low, mid);
-        mergeSort(a, mid + 1, high);
-        merge(a, low, mid, high);
+void merge(int a[], int lb, int mid, int ub) {
+    int i, j, k;
+    int n1 = mid - lb + 1;
+    int n2 = ub - mid;
+    
+    int LeftArray[n1], RightArray[n2];
+    
+    for (i = 0; i < n1; i++)
+        LeftArray[i] = a[lb + i];
+    for (j = 0; j < n2; j++)
+        RightArray[j] = a[mid + 1 + j];
+    
+    i = 0;
+    j = 0;
+    k = lb;
+    
+    while (i < n1 && j < n2) {
+        if (LeftArray[i] <= RightArray[j]) {
+            a[k] = LeftArray[i];
+            i++;
+        } else {
+            a[k] = RightArray[j];
+            j++;
+        }
+        k++;
+    }
+    
+    while (i < n1) {
+        a[k] = LeftArray[i];
+        i++;
+        k++;
+    }
+    
+    while (j < n2) {
+        a[k] = RightArray[j];
+        j++;
+        k++;
     }
 }
 
-void merge(int a[], int low, int mid, int high) {
-    int res[high - low + 1];
-    int i = low, j = mid + 1, k = 0;
-    while (i <= mid && j <= high) {
-        if (a[i] < a[j]) {
-            res[k++] = a[i++];
-        } else {
-            res[k++] = a[j++];
-        }
+void mergeSort(int a[], int lb, int ub) {
+    if (lb < ub) {
+        int mid = (lb + ub) / 2;
+        mergeSort(a, lb, mid);
+        mergeSort(a, mid + 1, ub);
+        merge(a, lb, mid, ub);
     }
-    while (i <= mid) {
-        res[k++] = a[i++];
-    }
-    while (j <= high) {
-        res[k++] = a[j++];
-    }
-    for (int p = 0; p < k; ++p) {
-        a[low + p] = res[p];
+}
+
+void mergeSort(int a[], int beg, int end) {
+    if (beg < end) {
+        int mid = (beg + end) / 2;
+        mergeSort(a, beg, mid);
+        mergeSort(a, mid + 1, end);
+        merge(a, beg, mid, end);
     }
 }
 
 int main() {
-    cout << "Enter the number of files: ";
-    int size;
-    cin >> size;
+    int a[] = {5, 9, 2, 7, 1, 3};
+    int n = sizeof(a) / sizeof(a[0]);
     
-    int array[size];
-    cout << "Enter the elements : \n";
-    for (int i = 0; i < size; i++) {
-        cin >> array[i];
-    }
-
+    cout << "Elements before sorting: ";
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    
     mergeSort(a, 0, n - 1);
-
-    cout << "Sorted elements :\n";
-    for (int i = 0; i < n; ++i) {
-        cout << a[i] << endl;
-    }
+    
+    cout << "\nElements after sorting: ";
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    
     return 0;
 }
