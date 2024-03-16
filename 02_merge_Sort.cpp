a4b7c95dae8c5988b12cf92a4f1d574b02e75dbc
srinivas k <124112058+srinivas-skr@users.noreply.github.com>
2024-03-16 15:55:56 +0530

    Update 02_merge_Sort.cpp

--- 02_merge_Sort.cpp
+++ 02_merge_Sort.cpp
@@ -1,35 +1,55 @@
 #include <iostream>
-#include <algorithm>
+
 using namespace std;
 
-void MergeSort(int arr[], int lb, int ub) {
-    if (lb < ub) {
-        int mid = lb + (ub - lb) / 2;
-        MergeSort(arr, lb, mid);
-        MergeSort(arr, mid + 1, ub);
-        inplace_merge(arr + lb, arr + mid + 1, arr + ub + 1); // to merge two sorted sub-arrays
+void merge(int a[], int low, int mid, int high);
+
+void mergeSort(int a[], int low, int high) {
+    if (low < high) {
+        int mid = (low + high) / 2;
+        mergeSort(a, low, mid);
+        mergeSort(a, mid + 1, high);
+        merge(a, low, mid, high);
     }
 }
 
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+void merge(int a[], int low, int mid, int high) {
+    int res[high - low + 1];
+    int i = low, j = mid + 1, k = 0;
+    while (i <= mid && j <= high) {
+        if (a[i] < a[j]) {
+            res[k++] = a[i++];
+        } else {
+            res[k++] = a[j++];
+        }
+    }
+    while (i <= mid) {
+        res[k++] = a[i++];
+    }
+    while (j <= high) {
+        res[k++] = a[j++];
+    }
+    for (int p = 0; p < k; ++p) {
+        a[low + p] = res[p];
     }
 }
 
 int main() {
+    cout << "Enter the number of files: ";
     int size;
-    cout << "Enter size of the array: ";
     cin >> size;
     
     int array[size];
-    cout << "Enter " << size << " numbers: ";
+    cout << "Enter the elements : \n";
     for (int i = 0; i < size; i++) {
         cin >> array[i];
     }
 
-    MergeSort(array, 0, size - 1);
-    cout << "Array after merge sort: ";
-    printArray(array, size);
+    mergeSort(a, 0, n - 1);
+
+    cout << "Sorted elements :\n";
+    for (int i = 0; i < n; ++i) {
+        cout << a[i] << endl;
+    }
     return 0;
 }
