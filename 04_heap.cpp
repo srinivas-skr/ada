b4bb076924b3719b6a42dd44483f740cbe00fb1e
srinivas k <124112058+srinivas-skr@users.noreply.github.com>
2024-03-18 13:55:55 +0530

    Update 04_heap.cpp

--- 04_heap.cpp
+++ 04_heap.cpp
@@ -6,8 +6,10 @@ void heapify(int arr[], int n, int i) {
     int left = 2 * i + 1;
     int right = 2 * i + 2;
 
-    if (left < n && arr[left] > arr[largest]) largest = left;
-    if (right < n && arr[right] > arr[largest]) largest = right;
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+    if (right < n && arr[right] > arr[largest]) 
+        largest = right;
 
     if (largest != i) {
         swap(arr[i], arr[largest]);
