ef9d5f7c1e9f2fa2df6b9e90d555c8cebb65a08b
srinivas k <124112058+srinivas-skr@users.noreply.github.com>
2024-03-18 16:34:22 +0530

    Update 04_heap.cpp

--- 04_heap.cpp
+++ 04_heap.cpp
@@ -34,7 +34,7 @@ int main() {
 
     int arr[size];
     cout << "Enter " << size << " numbers: ";
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < size; i++) {
         cin >> arr[i];
     }
 
