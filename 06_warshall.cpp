26e4b4be5d78446f5100fabbbbc97054e15deba6
srinivas k <124112058+srinivas-skr@users.noreply.github.com>
2024-04-12 16:34:42 +0530

    Update 06_warshall.cpp

--- 06_warshall.cpp
+++ 06_warshall.cpp
@@ -1,43 +1,30 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-void warshall(int a[20][20], int n);    // Function declaration
-
-int main() {
-    int n;
-    cout << "Enter the number of vertices: ";
-    cin >> n;
- 
-    int a[20][20];        // Array declaration for adjacency matrix
-    cout << "Enter the adjacency matrix (0/1):\n";
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> a[i][j];
-        }
+void warshall(int a[][20], int n){
+    for(int k = 0; k < n; k++)
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                a[i][j] = a[i][j] || (a[i][k] && a[k][j]);
+    
+    cout<<"\nPath matrix: "<<endl;
+    for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++)
+                cout<<a[i][j]<<"\t";
+            cout<<endl;
     }
-    cout << endl;
-    warshall(a, n);       // Call the function to compute and print the transitive closure
+}
+
+int main(){
+    int n, a[20][20];
+    cout<<"No. of vertices: ";
+    cin>>n;
+    
+    cout<<"\adjacency matrix: ";
+    for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                cin>>a[i][j];
+    
+    warshall(a, n);
     return 0;
 }
-
-void warshall(int a[20][20], int n) {    // Function definition
-    // Applying Warshall's algorithm
-    for (int k = 0; k < n; k++)
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                a[i][j] = a[i][j] || (a[i][k] && a[k][j]);
-
-    cout << "\nTransitive closure matrix: \n";    // Printing the transitive closure matrix
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++)
-            cout << a[i][j] << "\t";
-        cout << endl;
-    }
-}
-
-/* output
-Enter the number of vertices: 3
-Enter the adjacency matrix (0/1):
-0 1 0
-0 0 1
-1 0 0*/
