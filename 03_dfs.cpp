a3724d958a33c2fe48673d532c62cad4ec034e86
srinivas k <124112058+srinivas-skr@users.noreply.github.com>
2024-04-12 16:37:10 +0530

    Update 03_dfs.cpp

--- 03_dfs.cpp
+++ 03_dfs.cpp
@@ -1,48 +1,51 @@
 #include<iostream>
 using namespace std;
-
-int visited[20], a[20][20], n, i, v;
+int visited[20],a[20][20],n,i,v;
 
 void dfs(int v) {
     visited[v] = 1;
-    cout << v << " ";
-    for (i = 1; i <= n; i++) {
-        if (a[v][i] && !visited[i]) {
+    for(i=1; i<=n; i++) {
+        if(a[v][i] && !visited[i]) {
+            cout<<v<<"->"<<i<<endl;
             dfs(i);
         }
-    }
+    }  
 }
 
-int main() {
-    int count = 0;
-    n = 4; // Number of vertices
-    // Define the adjacency matrix for the predefined graph
-    int adjMatrix[4][4] = {
-        {0, 1, 1, 0},
-        {1, 0, 0, 1},
-        {1, 0, 0, 1},
-        {0, 1, 1, 0}
-    };
-    // Copy the predefined adjacency matrix to the actual adjacency matrix
-    for (int i = 1; i <= n; i++) {
-        visited[i] = 0;
-        for (int j = 1; j <= n; j++) {
-            a[i][j] = adjMatrix[i - 1][j - 1];
-        }
+int main()
+{
+ int count = 0;
+ cout<<"Enter no of vertices: ";
+ cin>>n;
+ for(int i=1; i<=n; i++) {
+    visited[i] = 0;
+    for(int j=1; j<=n; j++) {
+        a[i][j] = 0;
     }
-    cout << "DFS Traversal: ";
-    dfs(1); // Start DFS from vertex 1
-
-    for (int i = 1; i <= n; i++) {
-        if (visited[i]) {
-            count++;
-        }
-    }
+ }
     
-    if (count == n) {
-        cout << "\nGraph is connected. ";
-    } else {
-        cout << "\nGraph is disconnected. ";
+ cout<<"Enter adjacency matrix: ";
+ for(int i=1;i<=n;i++) {
+    for(int j=1; j<=n; j++) {
+        cin>>a[i][j];
     }
-    return 0;
+ }
+    
+ cout<<"Enter starting vertex: ";
+ cin>>v;
+ dfs(v);
+ 
+ for(int i=1; i<=n; i++) {
+    if(visited[i]) {
+        count++;
+    }
+ }
+    
+ if (count == n) {
+    cout<<"Graph is connected. ";
+ }
+ else {
+    cout<<"Graph is disconnected. ";
+ }
+ return 0;
 }
