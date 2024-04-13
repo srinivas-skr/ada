8534229c1159ffec20d384f5499de443ba0a3e16
srinivas k <124112058+srinivas-skr@users.noreply.github.com>
2024-04-14 00:41:59 +0530

    Update 07_dijistra.cpp

--- 07_dijistra.cpp
+++ 07_dijistra.cpp
@@ -1,58 +1,46 @@
 #include<iostream>
 using namespace std;
-int n, a[20][20], s[20], d[20], v;
-
-void dijkstra() {
-    int u;
-    for(int i = 1; i <= n; i++) {         // Initialize distances and visited array
-        d[i] = a[v][i];                // Initialize distance from source to vertex i
-        s[i] = 0;                         // Mark vertex i as unvisited
-    }
-    d[v] = 0;                       // Distance from source to itself is 0
-    s[v] = 1;                      // Mark the source vertex as visited
-    for(int k = 2; k <= n; k++) {
-        int min = 99;                  // Initialize minimum distance to a large value
-
-        for(int i = 1; i <= n; i++) // Find the vertex with the minimum distance from the source among the unvisited vertices
-            if(s[i] == 0 && d[i] < min) {
-                min = d[i];  // Update minimum distance
-                u = i;       // Update vertex u to the one with minimum distance
-            }
-        
-        s[u] = 1;   // Mark vertex u as visited
-        for(int i = 1; i <= n; i++)  // Update distances for vertices adjacent to the selected vertex
-            if(s[i] == 0)
-                if(d[i] > d[u] + a[u][i])
-                    d[i] = d[u] + a[u][i];
-    }
-    
-    cout << "The shortest path from " << v << " is " << endl;   // Output shortest paths
-    for(int i = 1; i <= n; i++)
-        cout << v << "-->" << i << "=" << d[i] << endl;
-}
 
 int main() {
-    cout << "Vertices: ";
-    cin >> n;
-
-    cout << "\nCost matrix: ";
-    for(int i = 1; i <= n; i++)
-        for(int j = 1; j <= n; j++)
-            cin >> a[i][j];
+int n, i, j, a[10][10], d[10], s[10], v, k, min , u;
     
-    cout << "\nSource vertex: ";
-    cin >> v;
-    // Perform Dijkstra's algorithm
-    dijkstra();
-    return 0;
+cout<<"No. of vertices: ";
+cin>>n;
+    
+cout<<"\nCost matrix: ";
+for(i = 1;i<=n; i++)
+for(j=1; j<=n; j++)
+cin>>a[i][j];
+    
+cout<<"\nSource vertex: ";
+cin>>v;
+    
+for(i = 1; i<=n; i++){
+s[i] = 0;
+d[i] = a[v][i];
 }
-
-/* OUTPUT
-Vertices: 4
-Cost matrix:
-0 10 3 999
-10 0 1 2
-3 1 0 8
-999 2 8 0
-Source vertex: 1 
-*/
+    
+d[v] = 0;
+s[v] = 1;
+    
+for(k = 2; k<=n; k++){
+min = 99;
+for(int i = 1; i<=n; i++){
+if(s[i]==0 && d[i] < min){
+min = d[i];
+u = i;
+}
+}
+    
+s[u] = 1;
+for(i = 1; i<=n; i++) if(s[i]==0){
+if(d[i] > (d[u] + a[u][i]))
+d[i] = d[u] + a[u][i];
+}
+}
+    
+cout<<"\nShortest distance from "<<v<<" is "<<endl;
+for (i = 2; i<=n; i++)
+cout<<v<<"--->"<<i<<"="<<d[i]<<endl;
+    
+}
