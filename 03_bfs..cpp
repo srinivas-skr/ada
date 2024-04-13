d7c28a7306c89781d8fc7bcdd2d9a566d6604709
srinivas k <124112058+srinivas-skr@users.noreply.github.com>
2024-04-13 22:19:31 +0530

    Update 03_bfs..cpp

--- 03_bfs..cpp
+++ 03_bfs..cpp
@@ -5,7 +5,7 @@ int adj[20][20], q[20], visited[20], n, i, j, f = 0, r = -1;
 
 void bfs(int v) {
     for (i = 1; i <= n; i++)
-        if (a[v][i] && !visited[i])
+        if (adj[v][i] && !visited[i])
             q[++r] = i;
     if (f <= r) {
         visited[q[f]] = 1;
